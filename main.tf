940da795f8c39a6e965f4aa9c7076fea76c5438b
peada04 <dpeacock@hashicorp.com>
2025-02-18 12:04:04 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,6 @@ provider "vault" {
 data "vault_aws_access_credentials" "aws_creds" {
   backend = "aws"
   role     = "my-role"
-  type     = "iam_user"
 }
 
 # configure provider to use Vault's dynamically generated credentials for AWS
