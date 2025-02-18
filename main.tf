648585e6459615027ab0e33f79549ed3ada44609
peada04 <dpeacock@hashicorp.com>
2025-02-18 12:13:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ terraform {
 provider "vault" {
   // skip_child_token must be explicitly set to true as HCP Terraform manages the token lifecycle
   skip_child_token = true
-  address          = var.tfc_vault_address
-  namespace        = var.tfc_vault_namespace
+  address          = var.tfc_vault_dynamic_credentials.default.address
+  namespace        = var.tfc_vault_dynamic_credentials.default.namespace
 
 //  auth_login_token_file {
 //    filename = var.tfc_vault_dynamic_credentials.default.token_filename
